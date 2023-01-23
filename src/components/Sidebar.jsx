4ab0c60865f9b3a18b9bf35424b0c1c0be2e433e
Emerson Araujo <emersonsantos@gmail.com>
2023-01-23 16:00:40 -0300

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,13 +6,15 @@ import InformationContainer from './InformationContainer';
 import SocialNetworks from './SocialNetworks';
 
 const Sidebar = () => {
+
+  const description = 'Emerson Araujo.pdf'
   return (
     <aside id="sidebar">
         <img src={Avatar} alt="Emerson Araújo" />
         <p className="title">Desenvolvedor Front-End</p>
         <SocialNetworks/>
         <InformationContainer/>
-        <a href={Curriculo} download="Emerson Araujo.pdf" className="btn"><span>Download currículo <FaDownload/></span><i></i></a>
+        <a href={Curriculo} download={description} className="btn"><span>Download currículo <FaDownload/></span><i></i></a>
     </aside>
   )
 }
